1725b029d37c5c87ee43fb1eda4af687ffd386d0
punkcow <briansquashic@gmail.com>
2015-07-14 20:19:51 -0400

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# tumblr
-practice tumblr clone django
--- dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-	url(r'^$', views.dashboard, name='dashboard'),
-	url(r'^logout$', views.log_out, name='log_out'),
-]
