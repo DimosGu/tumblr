16d48caf0e06b3ecd93cb2fe8f144ba135bb423d
punkcow <briansquashic@gmail.com>
2015-09-01 19:23:51 -0400

    added create_tags method

--- search/models.py
+++ search/models.py
@@ -10,6 +10,24 @@ class TagsManager(BaseManager):
   def filter_by_post(self, post):
     return self.filter(post=post)
 
+  def create_tags(self, new_tags, post):
+    stripped_tags = new_tags.replace('#', '').strip()
+    split_tags = stripped_tags.split(' ')
+
+    for tag in split_tags:
+
+      if tag == '':
+        pass
+      else:
+
+        try:
+          tags = Tags.objects.get_tag(tag)
+          tags.post.add(post)
+        except:
+          tags = Tags(tags=tag)
+          tags.save()
+          tags.post.add(post)
+
 class Tags(BaseModel):
 
   tags = models.CharField(max_length=20, blank=True)
