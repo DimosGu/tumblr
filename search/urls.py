5a2375824013e5e4149cbd3fb8c84e21c8f40b37
punkcow <briansquashic@gmail.com>
2015-09-02 23:55:42 -0400

    results url now accepts spaces in the regex

--- search/urls.py
+++ search/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.search, name='search'),
+  url(r'^(?P<results>[\w +]+)$', views.results, name='results'),
   url(r'^get_ten_posts/(?P<results>\w+)$', views.get_ten_posts, name='get_ten_posts'),
-  url(r'^(?P<results>\w+)$', views.results, name='results'),
 ]
