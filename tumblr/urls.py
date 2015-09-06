1b51930a193915fbebd9512c91814bcdca1ecb5f
punkcow <briansquashic@gmail.com>
2015-09-06 16:44:05 -0400

    changed app url paths

--- tumblr/urls.py
+++ tumblr/urls.py
@@ -4,12 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-	url(r'^', include('no_user.urls')),
-  url(r'^search/', include('search.urls')),
-	url(r'^sites/', include('sites.urls')),
-	url(r'^blog/', include('blog.urls')),
-	url(r'^explore/', include('explore.urls', namespace="explore")),
-	url(r'^user_accounts/', include('user_accounts.urls')),
+	url(r'^', include('apps.no_user.urls')),
+  url(r'^search/', include('apps.search.urls')),
+	url(r'^sites/', include('apps.sites.urls')),
+	url(r'^blog/', include('apps.blog.urls')),
+	url(r'^explore/', include('apps.explore.urls', namespace="explore")),
+	url(r'^user_accounts/', include('apps.user_accounts.urls')),
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'^dashboard/', include('dashboard.urls', namespace='dashboard')),
+	url(r'^dashboard/', include('apps.dashboard.urls', namespace='dashboard')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
