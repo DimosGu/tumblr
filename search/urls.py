e330fc5742d3d416042998cdf871ffae2d20063c
punkcow <briansquashic@gmail.com>
2015-09-06 16:46:18 -0400

    moved apps

--- search/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-  url(r'^$', views.search, name='search'),
-  url(r'^(?P<results>[\w\s+]+)$', views.results, name='results'),
-  url(r'^get_ten_posts/(?P<results>[\w\s+]+)$', views.get_ten_posts, name='get_ten_posts'),
-]
--- user_accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-	url(r'^logout$', views.log_out, name='log_out'),
-]
