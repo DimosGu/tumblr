d296ff6c0e5aff97d78897b3db2515d25feb58aa
punkcow <briansquashic@gmail.com>
2015-09-13 19:11:23 -0400

    added posts for the error images in the 404 template

--- tumblr/views.py
+++ tumblr/views.py
@@ -3,14 +3,14 @@ from apps.blog.models import Post
 from random import randint
 
 def error404(request):
-  pk_list = [1, 2]
-  error_images = Post.objects.filter_by_pk(pk_list)
+  pk_posts = [1, 8, 9, 12, 15, 18]
+  error_images = Post.objects.filter_by_pk(pk_posts)
   context = {}
   context['section'] = 'error'
   context['page_title'] = 'Not found.'
 
   try:
-    context['recent_img_post'] = error_images[randint(0, 4)]
+    context['recent_img_post'] = error_images[randint(0, 5)]
   except:
     context['recent_img_post'] = None
 
