da7abdf1b0a881da6b0d496aa93c4001c2b8d591
punkcow <briansquashic@gmail.com>
2015-09-19 14:34:49 -0400

    re-added lower case conversion method to username

--- apps/user_accounts/forms.py
+++ apps/user_accounts/forms.py
@@ -117,7 +117,7 @@ class RegistrationForm(forms.ModelForm):
 		return password
 
 	def clean_username(self):
-		username = self.cleaned_data["username"]
+		username = self.cleaned_data["username"].lower()
 		return username
 
 
