3ec9c1fec8f014cbbaffe8ed14d936d494663e5a
punkcow <briansquashic@gmail.com>
2015-09-22 22:45:55 -0400

    edited gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var gulp = require('gulp')
 
 // sass task
 gulp.task('build-sass', function () {
-  return gulp.src('./bluehorse/static/styles/scss/*.scss')
+  return gulp.src('./static/scss/*.scss')
         .pipe(sass({
           includePaths: ['styles'].concat(neat),
           onError: function (err) {
@@ -22,35 +22,35 @@ gulp.task('build-sass', function () {
         .pipe(cssimport({
           extensions: ["css"]
         }))
-        .pipe(gulp.dest('bluehorse/static/styles/css/'));
+        .pipe(gulp.dest('static/css/'));
 });
 
 
 gulp.task('minify-css', function() {
-  return gulp.src('./bluehorse/static/styles/css/*.css')
+  return gulp.src('./static/css/*.css')
     .pipe(minifyCss())
-    .pipe(gulp.dest('bluehorse/static/styles/css/'));
+    .pipe(gulp.dest('static/styles/css/'));
 });
 
 
-gulp.task('build-js', function(){
-  var files = [ './bluehorse/static/js/admin.js', './bluehorse/static/js/public.js' ];
-  var b = browserify(files);
-  b.plugin('factor-bundle', { outputs: [ './bluehorse/static/js/bundle/admin.js', './bluehorse/static/js/bundle/public.js' ] });
-  b.bundle().pipe(fs.createWriteStream('./bluehorse/static/js/bundle/common.js'));
-});
+// gulp.task('build-js', function(){
+//   var files = [ './static/js/admin.js', './static/js/public.js' ];
+//   var b = browserify(files);
+//   b.plugin('factor-bundle', { outputs: [ './static/js/bundle/admin.js', './bluehorse/static/js/bundle/public.js' ] });
+//   b.bundle().pipe(fs.createWriteStream('./static/js/bundle/common.js'));
+// });
 
 
-gulp.task('minify-js', function() {
-  return gulp.src('./bluehorse/static/js/bundle/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('bluehorse/static/js/bundle/'));
-});
+// gulp.task('minify-js', function() {
+//   return gulp.src('./bluehorse/static/js/bundle/*.js')
+//     .pipe(uglify())
+//     .pipe(gulp.dest('bluehorse/static/js/bundle/'));
+// });
 
 
 gulp.task('watch', function() {
-  gulp.watch(['./bluehorse/static/styles/scss/*.scss', './bluehorse/static/styles/scss/**/*.scss'], ['build-sass']);
-  gulp.watch(['./bluehorse/static/js/**/*.js', '!./bluehorse/static/js/bundle/*.js'], ['build-js']);
+  gulp.watch(['./static/styles/scss/*.scss', './static/styles/scss/**/*.scss'], ['build-sass']);
+  // gulp.watch(['./static/js/**/*.js', '!./static/js/bundle/*.js'], ['build-js']);
 });
 
 
